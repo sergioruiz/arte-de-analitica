37fb8fd201727c10a2171bc6b9bdfaf276f5a811
sergioruiz <4043216+sergioruiz@users.noreply.github.com>
2020-10-28 11:15:40 -0600

    Update kMeans.py

--- kMeans.py
+++ kMeans.py
@@ -6,11 +6,18 @@
 import random
 
 def distance2(a, b):
+    sum = 0
+    for i in range(len(b)):
+        sum += (b[i] - a[i])**2
+    return sum
+
+    '''
     dx = b[0] - a[0]
     dy = b[1] - a[1]
     dx2 = dx * dx
     dy2 = dy * dy
     return dx2 + dy2
+    '''
 
 def kMeans(data, centroids):
     centroidsAsKeys = []
@@ -39,10 +46,16 @@ def updateCentroids(theDict):
     newCentroids = []
     for key, pointsList in theDict.items():
         oldCentroid = key
-        newCentroid = [pointsList[0][0], pointsList[0][1]]
+        components = len(pointsList[0])
+        newCentroid = []
+        for c in range(components):
+            newCentroid.append(pointsList[0][c])   
+        #newCentroid = [pointsList[0][0], pointsList[0][1]]
         for p in pointsList:
-            newCentroid[0] = newCentroid[0] + p[0] / 2.0
-            newCentroid[1] = newCentroid[1] + p[1] / 2.0
+            for c in range(components):
+                newCentroid[c] = newCentroid[c] + p[c] / 2.0
+            #newCentroid[0] = newCentroid[0] + p[0] / 2.0
+            #newCentroid[1] = newCentroid[1] + p[1] / 2.0
         #print("old:",oldCentroid,"new:",newCentroid)
         newCentroids.append(newCentroid)
     return newCentroids
@@ -78,10 +91,22 @@ def initCentroids(data, k):
     #print()   
     return centroidsDict
 
-data = []
-for i in range(10):
-    point = [random.uniform(-10.0, 10.01), random.uniform(-10.0, 10.01)]
-    data.append(point)
+data = [
+[-2.273284107, 1.898485259],
+[9.285248345,  9.528708054],
+[-8.803332803,-1.148882298],
+[4.925078368,  2.331671642],
+[2.586415013,  1.28128069],
+[8.084455493, -0.9130888578],
+[0.9276023375, 0.5423613255],
+[-7.445467699,-0.7961183742],
+[-4.258454366, 9.335165888],
+[1.554880819, -2.925452212]]
+
+#data=[]
+#for i in range(10):
+#    point = [random.uniform(-10.0, 10.01), random.uniform(-10.0, 10.01)]
+#    data.append(point)
 print("Initial data:")
 print(data)
 print()
@@ -94,7 +119,7 @@ for key, value in cd.items():
     print()
 print()
 
-for i in range(5):
+for i in range(3):
     nc = updateCentroids(cd)
     print("New centers:")
     print(nc)
